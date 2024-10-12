23eec1e3b33da1434b972dcd728c584883220ef6
SAURABHVERMA00 <saurabh2271.be22@chitkara.edu.in>
2024-10-12 16:35:23 +0530

    feat: add Order Cart CartItem Service

---
+++ backend/src/services/cartItemService.js
@@ -0,0 +1,68 @@
+const CartItems = require("../models/cartItems.model");
+const userService = require("../services/userService");
+
+async function updateCartItem(userId, cartItemId, cartItemData) {
+  try {
+    const item = await CartItems.findCartItemById(cartItemId);
+
+    if (item.userId.toString() !== userId) {
+      throw new Error("You are not authorized to update this item", cartItemId);
+    }
+    const user = await userService.findUserById(item.userId);
+    if (!user) {
+      throw new Error("User not found", userId);
+    }
+
+    if (user._id.toString() === userId.toString()) {
+      item.quantity = cartItemData.quantity;
+      item.price = item.quantity * item.product.price;
+      item.discountedPrice = item.quantity * item.product.discountedPrice;
+
+      const updatedCartItem = await item.save();
+      return updatedCartItem;
+    } else {
+      throw new Error("You are not authorized to update this item", cartItemId);
+    }
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+async function removeCartItem(userId, cartItemId) {
+  try {
+    const cartItem = await findCartItemById(cartItemId);
+
+    if (cartItem.userId.toString() !== userId.toString()) {
+      throw new Error("You are not authorized to update this item", cartItemId);
+    }
+    const user = await userService.findUserById(item.userId);
+    if (!user) {
+      throw new Error("User not found", userId);
+    }
+
+    if (user._id.toString() === cartItem.userId.toString()) {
+      const deletedCartItem = await cartItem.remove();
+      return deletedCartItem;
+    } else {
+      throw new Error("You are not authorized to update this item", cartItemId);
+    }
+  } catch (error) {}
+  throw new Error(error.message);
+}
+
+async function findCartItemById(cartItemId) {
+  try {
+    const cartItem = await CartItems.findById(cartItemId);
+    if(!cartItem){
+      throw new Error("Cart item not found", cartItemId);
+    }
+    return cartItem;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+module.exports = {
+  updateCartItem,
+  removeCartItem,
+  findCartItemById,
+};
--- backend/src/services/cartService.js
+++ backend/src/services/cartService.js
@@ -1,19 +1,78 @@
-const Cart = require('../models/cartModel.model.js');
+const CartItems = require("../models/cartItems.model.js");
+const Cart = require("../models/cartModel.model.js");
+const Product = require("../models/product.model.js");
 
 async function createCart(userId) {
-    try{
-        const cart=new Cart({
-            userId
-        })
-        const createdCart=await cart.save();
-        return createdCart;
-    }
-    catch(error){
-        throw new Error(error.message);
-    }
-
-
-    
+  try {
+    const cart = new Cart({
+      userId,
+    });
+    const createdCart = await cart.save();
+    return createdCart;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
 
-module.exports={createCart}
+async function findUserCart(userId) {
+  try {
+    const cart = await Cart.findOne({ userId });
+
+    const cartItems = await CartItems.find({ cart: cart._id }).populate(
+      "product"
+    );
+    cart.cartItems = cartItems;
+
+    let totalPrice = 0;
+    let totalDiscountedPrice = 0;
+    let totalItem = 0;
+
+    for (let cartItem of cartItems) {
+      totalPrice += cartItem.price;
+      totalDiscountedPrice += cartItem.discountedPrice;
+      totalItem += cartItem.quantity;
+    }
+
+    cart.totalPrices = totalPrice;
+    cart.totalDiscountedPrice = totalPrice - totalDiscountedPrice;
+    cart.totalItems = totalItem;
+
+    return cart;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+async function addCartItem(userId, req) {
+  try {
+    const cart = await Cart.findOne({ user: userId });
+    const product = await Product.findById(req.productId);
+
+    const isPresent = await CartItems.findOne({
+      cart: cart._id,
+      product: product._id,
+      userId,
+    });
+    
+    if (!isPresent) {
+      const cartItem = new CartItems({
+        cart: cart._id,
+        product: product._id,
+        size: req.size,
+        quantity: 1,
+        price: product.price,
+        discountedPrice: product.discountedPrice,
+        userId,
+      });
+
+      const createCartItem = await cartItem.save();
+      cart.cartItems.push(createCartItem);
+
+      await cart.save();
+      return "Item added to Cart";
+    }
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+module.exports = { createCart, findUserCart };
